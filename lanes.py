e491102293fde13b7a37338028f4925be1c432fa
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2020-02-02 15:55:13 -0800

    adds lane detection for the video

--- lanes.py
+++ lanes.py
@@ -50,15 +50,36 @@ def region_of_interest(image):
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-image = cv2.imread('test_image.jpg')
-lane_image = numpy.copy(image)
-canny_image = canny(lane_image)
-cropped_image = region_of_interest(canny_image)
-lines = cv2.HoughLinesP(cropped_image, 2, numpy.pi/180, 100, numpy.array([]), minLineLength=40, maxLineGap=5)
-averaged_lines = average_slope_intercept(lane_image, lines)
-line_image = display_lines(lane_image, averaged_lines)
-combined_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+# uncomment this section to detect lanes in the image
 
-# cv2.imshow('result', region_of_interest(combined_image))
-cv2.imshow('result', combined_image)
-cv2.waitKey(0) == ord('q')
+# image = cv2.imread('test_image.jpg')
+# lane_image = numpy.copy(image)
+# canny_image = canny(lane_image)
+# cropped_image = region_of_interest(canny_image)
+# lines = cv2.HoughLinesP(cropped_image, 2, numpy.pi/180, 100, numpy.array([]), minLineLength=40, maxLineGap=5)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+# line_image = display_lines(lane_image, averaged_lines)
+# combined_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
+# cv2.imshow('result', region_of_interest(combined_image)) # something is wrong with this line
+
+# cv2.imshow('result', combined_image)
+# cv2.waitKey(0) == ord('q')
+
+video_capture = cv2.VideoCapture('test2.mp4')
+
+while(video_capture.isOpened()):
+    _, current_video_frame = video_capture.read()
+    canny_image = canny(current_video_frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, numpy.pi/180, 100, numpy.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(current_video_frame, lines)
+    line_image = display_lines(current_video_frame, averaged_lines)
+    combined_image = cv2.addWeighted(current_video_frame, 0.8, line_image, 1, 1)
+    # cv2.imshow('result', region_of_interest(combined_image))
+    cv2.imshow('result', combined_image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+current_video_frame.release()
+cv2.destroyAllWindows()
